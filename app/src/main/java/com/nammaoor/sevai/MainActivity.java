073b2d0c68fc3c01ed94fe896cc9fe3c77e03806
gopinathgg55-dot <gopinath.gg55@gmail.com>
2026-09-20 08:17:39 +0530

    Update MainActivity to show welcome message
    Replaced the default layout with a TextView displaying a welcome message.

--- app/src/main/java/com/nammaoor/sevai/MainActivity.java
+++ app/src/main/java/com/nammaoor/sevai/MainActivity.java
@@ -2,11 +2,21 @@ package com.nammaoor.sevai;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.view.Gravity;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.nammaoor.sevai.R.layout.activity_main);
+
+        TextView text = new TextView(this);
+        text.setText("நம்ம ஊர் சேவை ஆப் தயார்!\n\nWelcome to Namma Oor Sevai");
+        text.setTextSize(24);
+        text.setTextColor(Color.BLACK);
+        text.setGravity(Gravity.CENTER);
+
+        setContentView(text);
     }
 }
