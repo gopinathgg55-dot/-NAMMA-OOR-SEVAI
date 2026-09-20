62927bc89c79c66196dd4fad2a7202f1a8c73930
gopinathgg55-dot <gopinath.gg55@gmail.com>
2026-09-20 08:44:29 +0530

    Update MainActivity to use activity_main layout
    Replaced TextView with activity_main layout.

--- app/src/main/java/com/nammaoor/sevai/MainActivity.java
+++ app/src/main/java/com/nammaoor/sevai/MainActivity.java
@@ -2,21 +2,11 @@ package com.nammaoor.sevai;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.graphics.Color;
-import android.view.Gravity;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView text = new TextView(this);
-        text.setText("நம்ம ஊர் சேவை ஆப் தயார்!\n\nWelcome to Namma Oor Sevai");
-        text.setTextSize(24);
-        text.setTextColor(Color.BLACK);
-        text.setGravity(Gravity.CENTER);
-
-        setContentView(text);
+        setContentView(R.layout.activity_main);
     }
 }
