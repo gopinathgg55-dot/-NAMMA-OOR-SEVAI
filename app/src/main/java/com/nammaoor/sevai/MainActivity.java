a36717a572e93c4f8f5688eb4e946d4b2042eca7
gopinathgg55-dot <gopinath.gg55@gmail.com>
2026-09-20 07:54:38 +0530

    Update layout reference in MainActivity

--- app/src/main/java/com/nammaoor/sevai/MainActivity.java
+++ app/src/main/java/com/nammaoor/sevai/MainActivity.java
@@ -7,7 +7,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.nammaoor.sevai.R.layout.activity_main);
     }
 }
-
